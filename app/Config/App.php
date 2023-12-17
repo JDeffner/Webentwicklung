c212ce318c444595befa690c4b34ffb95939fa66
talina2 <s4tadara@uni-trier.de>
2023-12-17 20:51:16 +0100

    Uebung04

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,8 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'team04.wi1cm.uni-trier.de/public/';
+    //public string $baseURL = 'team04.wi1cm.uni-trier.de/public/';
+    public string $baseURL = 'http://localhost/web-entwicklung/public';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
