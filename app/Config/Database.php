10d92bff1615d54c495744538f268ecf19725fa6
JoelDeffner <134447802+JDeffner@users.noreply.github.com>
2024-01-22 23:59:08 +0100

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'team04',
-        'password'     => 'kGOt_team04',
-        'database'     => 'db_team04',
+        'username'     => '',
+        'password'     => '',
+        'database'     => '',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
