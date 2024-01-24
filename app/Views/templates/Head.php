493a9f9f0318d9a890ebc156e9f9a6571c61fc7a
JoelDeffner <134447802+JDeffner@users.noreply.github.com>
2024-01-24 11:44:04 +0100

    styling setup more

--- app/Views/templates/Head.php
+++ app/Views/templates/Head.php
@@ -6,7 +6,6 @@
     <!-- Added default title in case $title is not set-->
     <title><?= ($title ?? 'Title not set') ?></title>
     <link rel="icon" href="<?php echo base_url();?>resources/images/favicon.ico" type="image/x-icon">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.2/font/bootstrap-icons.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.22.1/bootstrap-table.min.css" rel="stylesheet">
     <link href="<?php echo base_url();?>resources/css/main.css" rel="stylesheet" type="text/css">
