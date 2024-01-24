6de54170018348a4da4dd7fbce9c424e95b3a0ae
JoelDeffner <134447802+JDeffner@users.noreply.github.com>
2024-01-24 12:47:34 +0100

    more imports update

--- app/Views/templates/Head.php
+++ app/Views/templates/Head.php
@@ -5,15 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Added default title in case $title is not set-->
     <title><?= ($title ?? 'Title not set') ?></title>
-    <link rel="icon" href="resources/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="<?php echo base_url();?>resources/images/favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.2/font/bootstrap-icons.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.22.1/bootstrap-table.min.css" rel="stylesheet">
-    <link href="resources/css/main.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url();?>resources/css/main.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.22.1/bootstrap-table.min.js"></script>
-    <script defer src="resources/js/main.js"></script>
+    <script defer src="<?php echo base_url();?>resources/js/main.js"></script>
 </head>
 <body>
